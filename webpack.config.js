31c545fcf2d10365c920cce446a3b876f279910f
Matheus Rezende <matheus.appmasters@gmail.com>
2017-10-23 10:26:45 -0200

    bundle

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
+        sourceMapFilename: 'index.map',
         library: 'jslib',
         libraryTarget: 'umd'
     },
