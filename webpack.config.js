bf6b9cea47d78b9ff4e5b9055876cb2820a38bff
Matheus Rezende <matheus.appmasters@gmail.com>
2017-10-23 11:31:45 -0200

    fetch polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -8,11 +9,6 @@ module.exports = {
         library: 'jslib',
         libraryTarget: 'umd'
     },
-    externals: [
-        {
-            "window": "window"
-        }
-    ],
     module: {
         rules: [
             {
@@ -20,5 +16,11 @@ module.exports = {
                 use: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            'Promise': 'es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+            'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        })
+    ]
 };
