bf12fa8d444b4747f58aea7e84a6d6008f31a31a
Tiago Gouvêa <tiago@tiagogouvea.com.br>
2017-12-13 08:26:17 -0200

    Removing Matheus trash

--- dist/auth.actionTypes.js
+++ dist/auth.actionTypes.js
@@ -3,26 +3,18 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.ACTIONS = undefined;
-
-var _keymirror = require('keymirror');
-
-var _keymirror2 = _interopRequireDefault(_keymirror);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var ACTIONS = exports.ACTIONS = (0, _keymirror2.default)({
-    AUTH_INPUT_CHANGED: null,
-    AUTH_LOGIN_USER_SUCCESS: null,
-    AUTH_LOGIN_USER_FAIL: null,
-    AUTH_LOGIN_USER: null,
-    AUTH_SIGNUP_USER_SUCCESS: null,
-    AUTH_SIGNUP_USER_FAIL: null,
-    AUTH_SIGNUP_USER: null,
-    AUTH_IS_USER_AUTHENTICATED: null,
-    AUTH_LOGOUT_USER: null,
-    AUTH_EMAIL_FORMAT_ERROR: null,
-    AUTH_EMPTY_PASSWORD_ERROR: null,
-    AUTH_PASSWORD_UNMATCHED: null,
-    AUTH_ERROR: null
+var ACTIONS = exports.ACTIONS = keymirror({
+    AUTH_INPUT_CHANGED: 'AUTH_INPUT_CHANGED',
+    AUTH_LOGIN_USER_SUCCESS: 'AUTH_LOGIN_USER_SUCCESS',
+    AUTH_LOGIN_USER_FAIL: 'AUTH_LOGIN_USER_FAIL',
+    AUTH_LOGIN_USER: 'AUTH_LOGIN_USER',
+    AUTH_SIGNUP_USER_SUCCESS: 'AUTH_SIGNUP_USER_SUCCESS',
+    AUTH_SIGNUP_USER_FAIL: 'AUTH_SIGNUP_USER_FAIL',
+    AUTH_SIGNUP_USER: 'AUTH_SIGNUP_USER',
+    AUTH_IS_USER_AUTHENTICATED: 'AUTH_IS_USER_AUTHENTICATED',
+    AUTH_LOGOUT_USER: 'AUTH_LOGOUT_USER',
+    AUTH_EMAIL_FORMAT_ERROR: 'AUTH_EMAIL_FORMAT_ERROR',
+    AUTH_EMPTY_PASSWORD_ERROR: 'AUTH_EMPTY_PASSWORD_ERROR',
+    AUTH_PASSWORD_UNMATCHED: 'AUTH_PASSWORD_UNMATCHED',
+    AUTH_ERROR: 'AUTH_ERROR'
 });
